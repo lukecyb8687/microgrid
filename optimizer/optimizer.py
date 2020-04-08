bdfae8f730fe73c5869bdee02a38c518e0a2586d
Bastiche <bastien.velitchkine@live.fr>
2020-04-08 17:25:32 +0200

    Modifying the optimizerTest function

--- optimizer/optimizer.py
+++ optimizer/optimizer.py
@@ -99,58 +99,58 @@ def optimizerTest():
     A simple function to test the function `optimizer`
     """
     
-    fixedParameters = {"gridComponents": {
+    fixedParameters = {
+                            "gridComponents": {
                                                         "battery" : {
                                                                         "initialStorage": 1.,
                                                                         "maxInputPow": 6 * 157,
                                                                         "maxOutputPow": 6 * 500,
                                                                         "SOC_min": 0.,
-                                                                        "maxThroughput": float, the maximum amount of energy that can flow in and out of the battery during its lifetime (kWh),
-                                                                        "lifetime": int, the nominal lifetime of the battery in hours. It's the time after which we must replace it if we did not exceed the maximum throughput,
-                                                                        "capitalCost": float, the cost of a 1kWh battery in $,
-                                                                        "replacementCost": float, the cost to replace 1kWh of batteries in $,
-                                                                        "operationalCost": float, the cost PER HOUR, to operate and maintain the battery ($)
+                                                                        "maxThroughput": 3000.,
+                                                                        "lifetime": 5 * 365 * 24,
+                                                                        "capitalCost": 200,
+                                                                        "replacementCost": 200,
+                                                                        "operationalCost": 0.03
                                                                     },
                                                         "diesel" : {
-                                                                        "fuelCost": float, the cost of 1l of fuel ($),
-                                                                        "fuelCostGrad": float, there is a fuel curve modeling the relationship between the functionning power of the dg and it's consumption of liters fuel per kW. This parameter is the slope of the model curve,
-                                                                        "fuelCostIntercept": float, there is a fuel curve modeling the relationship between the functionning power of the dg and it's consumption of liters fuel per kW. This parameter is the intercept of the model curve,
-                                                                        "lifetime": int, the nominal lifetime of the dg in hours,
-                                                                        "capitalCost": float, the cost of the dg in $,
-                                                                        "replacementCost": float, the cost to replace the dg in $,
-                                                                        "operationalCost": float, the cost PER HOUR, to operate and maintain the dg,
+                                                                        "fuelCost": 1.2,
+                                                                        "fuelCostGrad": 1.,
+                                                                        "fuelCostIntercept": 0.,
+                                                                        "lifetime": 10 * 365 * 24,
+                                                                        "capitalCost": 1000,
+                                                                        "replacementCost": 1000,
+                                                                        "operationalCost": 0.03,
                                                                     },
                                                         "photovoltaic" : {
-                                                                            "lifetime": int, the nominal lifetime of a pannel in hours,
-                                                                            "capitalCost": float, the cost of a pannel in $,
-                                                                            "replacementCost": float, the cost to replace a pannel in $,
-                                                                            "operationalCost": float, the cost PER HOUR, to operate and maintain the pannel ($),
-                                                                            "powerTimeVector": numpy array, the power output of the pannel at each time step (kW)
+                                                                            "lifetime": 25 * 365 * 24,
+                                                                            "capitalCost": 500,
+                                                                            "replacementCost": 500,
+                                                                            "operationalCost": 0.03,
+                                                                            "powerTimeVector": np.array([abs(np.sin((2 * np.pi * hour/ 48))) for hour in np.arange(0, 24 * 365)]) # We suppose that the irradiance of the pannels is a sinusoide
                                                                         }
                                                     },
                                     
-                               timeStep : float, the time step of the simulation that generated the load in hours,
-                               loadVector : numpy array of floats, the power demand on the grid (kW),
-                               projectDuration : int, the duration of the whole project in hours (e.g 25 * 365 * 24),
-                               discountRate: float, the discount ratio,
-                               strategy : "LF" or "CC", respectively for "Load Following" or "Cycle Charging"
-                               }
-                - costFunctions : list of 
-                    - lambda functions whose inputs are the arguments to optimize and outputs are the the floats corresponding to the costs we aim at minimizing
-                - constraints : {
+                               "timeStep" : 1,
+                               "loadVector" : np.array([abs(np.sin((2 * np.pi * hour/ 24 - (np.pi/2)))) for hour in np.arange(0, 24 * 365)]), # We model the load by a sinusoide with max demand at 6 am and 6pm
+                               "projectDuration" : 25 * 365 * 24,
+                               "discountRate": 0.0588,
+                               "strategy" : "LF"
+                       }
+    constraints = {
                                     "diesel":
                                                 {
-                                                        "upperBound": float, the upper Bound for the value of the generatorMaximumPower (kW),
-                                                        "lowerBound": float, the lower Bound for the value of the generatorMaximumPower (kW),
+                                                        "upperBound": 100,
+                                                        "lowerBound": 0,
                                                 },
                                      "battery":
                                                 {
-                                                        "upperBound": float, the upper Bound for the value of the battery maximum storage capacity (kWh),
-                                                        "lowerBound": float, the lower Bound for the value of the battery maximum storage capacity (kWh),
+                                                        "upperBound": 100,
+                                                        "lowerBound": 0,
                                                 },
                                      "photovoltaic":
                                                     {
-                                                            "upperBound": float, the upper Bound for the value of the pvMaximumPower (kW),
-                                                            "lowerBound": float, the upperlower Bound for the value of the pvMaximumPower (kW),
+                                                            "upperBound": 100,
+                                                            "lowerBound": 0,
                                                     },           
                                 }
+    optimizer(fixedParameters, constraints)    
